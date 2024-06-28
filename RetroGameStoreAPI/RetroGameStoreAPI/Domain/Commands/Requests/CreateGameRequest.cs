63fabcfa767b5a7283c3af89a099cd91d1e480d8
jeanps22 <jean.philip22@hotmail.com>
2024-06-27 21:36:00 -0300

    Init Sln - Creating Customer and Game Controllers using CQRS and mediatR

---
+++ RetroGameStoreAPI/RetroGameStoreAPI/Domain/Commands/Requests/CreateGameRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using RetroGameStoreAPI.Domain.Commands.Responses;
+
+namespace RetroGameStoreAPI.Domain.Commands.Requests
+{
+    public class CreateGameRequest : IRequest<CreateGameResponse>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
