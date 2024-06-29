7c379b2c57517c672a6cecacda5d2812fa9158c7
jeanps22 <jean.philip22@hotmail.com>
2024-06-29 12:13:38 -0300

    Fix: Add Service config

--- RetroGameStoreAPI/RetroGameStoreAPI/Program.cs
+++ RetroGameStoreAPI/RetroGameStoreAPI/Program.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using RetroGameStoreAPI.Domain.Handlers;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<ICreateCustomerHandler, CreateCustomerHandler>();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- RetroGameStoreAPI/RetroGameStoreAPI/obj/Debug/net8.0/RetroGameStoreAPI.AssemblyInfo.cs
+++ RetroGameStoreAPI/RetroGameStoreAPI/obj/Debug/net8.0/RetroGameStoreAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetroGameStoreAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c18e26834e2fc6f281bbd4cd967a9674e429ae4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35192dfc7dea21643865e2d7321fee4f971f77ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetroGameStoreAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetroGameStoreAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
